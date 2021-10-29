715c7272c71e3a12efb3a9f4767397cf3a8908e3
Vinicius Santos <viniciusmain1@gmail.com>
2021-10-28 21:48:42 -0300

    Primeira versão aula HTML/CSS/JS

---
+++ js/codigo.js
@@ -0,0 +1,21 @@
+<script>
+      console.log("Teste de saída no console do browser");
+      console.log("Outra frase qualquer");
+      let nome;
+      nome = "Vinicius Santos";
+      console.log(nome);
+      nome = 57;
+      console.log(nome);
+      //Global
+      var idade = 21;
+      console.log(idade);
+      {
+        //bloco
+        let idade = 18;
+        console.log(idade);
+      }
+      console.log(idade);
+
+      const nome = "Vinicius";
+      console.log(nome);
+    </script>
